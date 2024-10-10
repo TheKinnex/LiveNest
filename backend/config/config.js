7e84b9b55e120e9d44b1f0a3c2bd1686a368ff22
Victor Hernandez <91023561+TheKinnex@users.noreply.github.com>
2024-10-10 18:56:50 -0400

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,10 +3,7 @@ import mongoose from 'mongoose'; // Importar usando ESM
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error(`Error: ${error.message}`);
