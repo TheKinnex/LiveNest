33d3504c74b5ea5a4d10c3d184c7dcd1a9e983e3
Victor Hernandez <victoriusd4@gmail.com>
2024-10-10 18:41:00 -0400

    Create Conversation.js

---
+++ backend/models/Conversation.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+
+/*
+  Ideas: Grupos
+*/
+const conversationSchema = new mongoose.Schema({
+  // Array de usuarios participantes en la conversación
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  ],
+  // Array de IDs de los mensajes de la conversación
+  messages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+      required: true,
+    },
+  ],
+  // Indicador de si uno de los participantes ha silenciado la conversación
+  isMuted: {
+    type: Boolean,
+    default: false,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Conversation = mongoose.model("Conversation", conversationSchema);
+module.exports = Conversation;
